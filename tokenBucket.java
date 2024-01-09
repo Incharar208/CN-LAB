6f88fa859036878b1d0db24f8533cd27b74f959c
Inchara R <116972102+Incharar208@users.noreply.github.com>
2024-01-10 00:59:00 +0530

    Final

--- tokenBucket.java
+++ tokenBucket.java
@@ -1,18 +1,22 @@
-import java.util.Scanner;
+/*
+Write a program for congestion control using token bucket algorithm.
+*/
 import java.util.*;
 
 public class tokenBucket {
-    public static void main(String []args) {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int tokenRequested, tokenSent, tokenRemaining = 0;
+        int tokenRequested, tokenSent , tokenRemaining=0;
         System.out.println("Enter the bucket capacity:");
         int bucketCapacity = in.nextInt();
         System.out.println("Enter the token generation rate:");
         int tokenGeneration = in.nextInt();
         System.out.println("Enter the number of cycles:");
         int n = in.nextInt();
-        System.out.println(String.format("%s\t%s\t%s\t%s","Time","Token Requested","Token Sent","Token Remaining"));
-        for(int i = 0; i < n; i++ )
+
+        System.out.println(String.format("%s\t%s\t%s\t%s","Time", "Token Requested", "Token Sent", "Token Remaining"));
+
+        for(int i = 0; i < n; i++)
         {
             tokenRequested = tokenGeneration;
             if(tokenRequested + tokenRemaining > bucketCapacity)
@@ -25,8 +29,24 @@ public class tokenBucket {
                 tokenSent = tokenRequested;
                 tokenRemaining = tokenRemaining + tokenRequested;
             }
-            System.out.println(String.format("%d\t\t%d\t\t%d\t\t%d",i + 1, tokenRequested, tokenSent, tokenRemaining));
+            System.out.println(String.format("%d\t%d\t\t%d\t\t%d",i + 1, tokenRequested, tokenSent, tokenRemaining));
         }
-
     }
 }
+
+//OUTPUT:
+/*
+Enter the bucket capacity:
+5
+Enter the token generation rate:
+2
+Enter the number of cycles:
+6
+Time    Token Requested Token Sent      Token Remaining
+1       2               2               2
+2       2               2               4
+3       2               1               5
+4       2               0               5
+5       2               0               5
+6       2               0               5
+*/
